0e98c8f31f89b6c0703aa43c8e66d1860e956158
Purna Shrestha <crush9918@gmail.com>
2023-10-16 19:49:09 +0545

    Create design 01 landing page issue #126

---
+++ 01Design_2023/purnasth/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        romance: ["Classical Romance", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
